4b82e1e35526aac82d302ae8813c7548d3675064
Somdev Mitra <somdev_55@outlook.com>
2022-06-26 15:39:03 +0530

    Done

--- index.js
+++ index.js
@@ -214,9 +214,9 @@ function change(num1){
 var changed=change(10)
 console.log(changed)
 function nextLine(arr,item){
+    arr.push(item)
 return item;}
 var testArr = [1,2,3,4,5];
 console.log("Before:"+JSON.stringify(testArr));
 console.log(nextInLine(testArr,6));
 console.log("After:"+JSON.stringify(testArr));
-
