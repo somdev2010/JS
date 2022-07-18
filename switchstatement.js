42b84251a67d4b259f4bc9ff2adfbd0808a1979c
Somdev Mitra <somdev_55@outlook.com>
2022-07-18 16:54:31 +0530

    MultipleSwitch

--- switchstatement.js
+++ switchstatement.js
@@ -19,3 +19,47 @@ console.log(caseInSwitch(3))
 console.log(caseInSwitch(4))
 console.log(caseInSwitch(1))
 console.log(caseInSwitch(2))
+function switchDefault(val2){
+    var result="";
+    switch(val2){
+        case "a":
+            result="Brute"
+                break;
+        case "b":
+            result="Radical"
+                break;
+        case "c":
+            result="Balanced"
+                break;
+        case "d":
+            result="Defensive"
+                break;
+        default:
+            result="Perfect"
+                break;
+    }
+    return result
+}
+console.log(switchDefault("b"));
+console.log(switchDefault(3))
+function MultipleOptionsinSwitch(val3){
+    var solution="";
+    switch(val3){
+    case 1:
+    case 2:
+    case 3:
+        solution="Top Order"
+    case 4:
+    case 5:
+    case 6:
+        solution="Middle Order"
+    case 7:
+    case 8:
+    case 9:
+        solution="Lower Order"
+    }
+    return solution;
+}
+console.log(MultipleOptionsinSwitch(8))
+console.log(MultipleOptionsinSwitch(3))
+console.log(MultipleOptionsinSwitch(6))
