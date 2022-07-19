997b4b671bd6025400c0929df39874b5fcdebde1
Somdev Mitra <somdev_55@outlook.com>
2022-07-19 19:20:24 +0530

    Dot Notation and Basics_last one for today

---
+++ BasicsofObjects.js
@@ -0,0 +1,17 @@
+//Basics of Objects
+var myDog={
+    "name":"Lucie",
+    "legs":4,
+    "tails":1,
+    "friends":["everything!"]
+};
+//Dot Notation
+var testObj={
+    "hat":"ballcap",
+    "shirt":"jersey",
+    "shoes":"cleats"
+};
+var hatValue=testObj.hat;
+var shirtValue=testObj.shirt
+console.log(hatValue)
+
