6d65fcc05669b0e114b6ff25db1059f947b06543
Somdev Mitra <somdev_55@outlook.com>
2022-07-27 20:00:47 +0530

    Loop_1

---
+++ Loops.js
@@ -0,0 +1,21 @@
+//For Loop:
+var ourlist=[];
+for (var i=0; i<5; i++)
+{
+    ourlist.push(i);
+}
+console.log(ourlist)
+var myListown=[]
+for (var a=0; a<50; a++){
+    myListown.push(a)
+}
+console.log(myListown)
+//Break-statement:
+var breakArray=[];
+for (var b=0; b<10; b++)
+{
+    if (b>2) break;
+    breakArray.push(b) 
+}
+console.log(breakArray)
+var arr=[]
